d27d337d91b57bfb2f0933441e4168f796465da1
Priyanshi Barmola <149074393+priyanshibarmola@users.noreply.github.com>
2026-06-13 13:16:49 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,15 @@
+require('dotenv').config()
+const mongoose = require("mongoose")
+
 exports.connectToDB = async () => {
     try {
-        console.log("URI exists:", !!process.env.MONGO_URI);
+        console.log("URI exists:", !!process.env.MONGO_URI)
 
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI)
 
-        console.log("connected to DB");
+        console.log("connected to DB")
     } catch (error) {
-        console.error("DB ERROR:");
-        console.error(error);
+        console.error("DB ERROR:")
+        console.error(error)
     }
 }
